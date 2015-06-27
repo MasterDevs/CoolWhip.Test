5e907bd9aeec69d9bae1c28eafcd19b60544be9c
Josh Quintus <josh@dotalign.com>
2015-06-27 12:53:21 -0400

    Super simple project to make use of cool whip.

---
+++ MasterDevs.CoolWhip.Test/MasterDevs.CoolWhip.Test/HandMixer.cs
@@ -0,0 +1,15 @@
+﻿namespace MasterDevs.CoolWhip.Test
+{
+    public class HandMixer
+    {
+        public string Mix(string ingredient1, string ingredient2)
+        {
+            if (ingredient1 == "cream" && ingredient2 == "sugar")
+            {
+            return "Cool Whip";
+            }
+
+            return "That's not how you make cool whip!";
+        }
+    }
+}
